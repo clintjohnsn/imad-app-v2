654e6740f9cfadfc05bd9c1cebb6546c386824f5
clintjohnsn <clint8875@gmail.com>
2017-03-18 13:19:10 +0530

    animated madi  img

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
-console.log('Loaded!');
+madi=document.getElementById('madi');
+
+moveAmount=0;
+move =function(){
+
+	moveAmount+=5;
+	madi.style.paddingLeft=moveAmount+'px';
+};
+madi.onclick=function(){
+	interval = setInterval(move,50);
+};
+
+console.log("daf");
